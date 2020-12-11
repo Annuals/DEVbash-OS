cc893c186d549a0ee0a94c28fd506abb70bbb8cc
Annuals <75097279+Annuals@users.noreply.github.com>
2020-12-11 13:25:05 -0500

    Rename

---
+++ build.sh
@@ -0,0 +1,10 @@
+if ! which nasm > /dev/null; then install-pkg nasm; fi
+if ! which qemu-system-i386 > /dev/null; then install-pkg qemu; fi
+
+
+nasm -f elf32 ./OS/kernel.asm -o ./OS/kernelasm.o
+gcc -m32 -c ./OS/kernel.c -o ./OS/kernelc.o
+ld -m elf_i386 -T link.ld -o ./OS/kernel.bin ./OS/kernelasm.o ./OS/kernelc.o
+
+
+qemu-system-i386 -kernel ./OS/kernel.bin
