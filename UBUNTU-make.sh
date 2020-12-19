5632d72a9f4db45129ffe3443b316dc67630bcf4
Annuals <75097279+Annuals@users.noreply.github.com>
2020-12-19 00:20:34 -0500

    Update UBUNTU-make.sh

--- UBUNTU-make.sh
+++ UBUNTU-make.sh
@@ -1,10 +1,6 @@
 if ! which nasm > /dev/null; then sudo apt-get install nasm; fi
 if ! which qemu-system-i386 > /dev/null; then sudo apt-get install qemu; fi
-
-
 nasm -f elf32 kernel.asm -o kernelasm.o
 gcc -m32 -c kernel.c -o kernelc.o
 ld -m elf_i386 -T link.ld -o kernel.bin kernelasm.o kernelc.o
-cat README/readme.md
-
 qemu-system-i386 -kernel kernel.bin
